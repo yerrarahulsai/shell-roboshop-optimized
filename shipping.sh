38fa96cd1bf073970a96d1c376a36189bd95791d
yerrarahulsai <rahuldevops1802@gmail.com>
2025-10-02 20:46:26 +0530

    Shipping setup file

---
+++ shipping.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=shipping
+
+check_root
+app_setup
+java_setup
+systemd_setup
+
+dnf install mysql -y &>>$LOG_FILE
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+else
+    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+fi
+
+app_restart
+total_time
