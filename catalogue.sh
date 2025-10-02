c29e0812c3ea8575a8c06f5f664f9a61f8311efb
yerrarahulsai <rahuldevops1802@gmail.com>
2025-10-02 16:53:55 +0530

    Catalogue setup file

---
+++ catalogue.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source ./common.sh
+app_name="catalogue"
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mmongo.repo
+validate $? "Copying Mongo Repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+validate $? "Install MongoDB client"
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load $app_name products"
+else
+    echo -e "$app_name products already loaded ... $Y SKIPPING $N"
+fi
+
+app_restart
+total_time
