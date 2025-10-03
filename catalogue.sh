9209287e78c768b669c0237d477b53ca4ac8caba
yerrarahulsai <rahuldevops1802@gmail.com>
2025-10-03 12:21:05 +0530

    Catalogue setup file

--- catalogue.sh
+++ catalogue.sh
@@ -14,7 +14,7 @@ validate $? "Copying Mongo Repo"
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 validate $? "Install MongoDB client"
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.rahulsai.com --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load $app_name products"
